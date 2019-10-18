50b1cfafeb62eaf8fabcb7e7dd2a75af868fa1a7
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-10-19 00:59:12 +0900

    pointer Update

--- Other/pointer.cpp
+++ Other/pointer.cpp
@@ -3,14 +3,39 @@
 using std::cout;
 using std::endl;
 
-//  ポインタ渡し
-void func(int *a)
+//  値渡し
+void valueFunc(int i)
 {
-    cout << a[0] << endl;
+    i++;
+
+    //  この関数内のiの値
+    cout << "valueFunc[i]値 : " << i << endl;
+    cout << "valueFunc[i]アドレス : " << &i << endl;
 }
 
+//  ポインタ渡し
+void pointerFunc(int *i)
+{
+    *i += 1;
+    cout << "main[i]アドレス : " << i << endl;
+    cout << "この関数内のmain[i]のポインタのアドレス : " << &i << endl;
+}
+
+//  参照渡し
+void referenceFunc(int &i)
+{
+    i++;
+    cout << "main[i]の値 : " << i << endl;
+    cout << "main[i]アドレス : " << &i << endl;
+}
 int main()
 {
-    int a[] = {3, 4, 5, 6};
-    func(a);
+    int i = 5;
+
+    cout << "main[i]の値 : " << i << endl;
+    cout << "main[i]アドレス : " << &i << endl;
+
+    valueFunc(i);
+    pointerFunc(&i);
+    referenceFunc(i);
 }
