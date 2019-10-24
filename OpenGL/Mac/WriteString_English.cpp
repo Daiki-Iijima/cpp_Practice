774f795234188d11458dbc0daa6102df1429ec5e
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-10-24 15:09:16 +0900

    文字列描画(日本語非対応)

---
+++ OpenGL/Mac/WriteString_English.cpp
@@ -0,0 +1,69 @@
+#include <GLUT/GLUT.h>
+#include <OpenGL/OpenGL.h>
+#include <string>
+
+#define WIDTH 320
+#define HEIGHT 240
+
+static void DrawString(std::string str, int w, int h, int x0, int y0)
+{
+	glDisable(GL_LIGHTING);
+	// 平行投影にする
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, w, h, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+
+	// 画面上にテキスト描画
+	glRasterPos2f(x0, y0);
+	int size = (int)str.size();
+	for (int i = 0; i < size; ++i)
+	{
+		char ic = str[i];
+		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ic);
+	}
+
+	glPopMatrix();
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void display(void)
+{
+	//	塗りつぶす色を指定
+	glClearColor(0.0, 1.0, 1.0, 1.0);
+
+	glColor3d(1.0, 0.0, 1.0);
+	DrawString("test", WIDTH, HEIGHT, 10, 10);
+
+	//	リフレッシュ
+	glFlush();
+}
+
+int main(int argc, char *argv[])
+{
+	//	ウィンドウの生成位置を指定
+	glutInitWindowPosition(100, 100);
+
+	//	ウィンドウの生成サイズを指定
+	glutInitWindowSize(640, 480);
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_RGBA);
+
+	glutCreateWindow("A Ho New World");
+
+	glutDisplayFunc(display);
+
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+
+	glutMainLoop();
+
+	return 0;
+}
