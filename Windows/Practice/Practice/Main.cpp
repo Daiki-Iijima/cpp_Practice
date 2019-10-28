c76dbe386c4680e9e4194ccca3114f3807ff4a8f
daiki_iijima <daiki.applecreate@gmail.com>
2019-10-29 00:46:54 +0900

    Window表示(glut最小環境)

--- Windows/Practice/Practice/Main.cpp
+++ Windows/Practice/Practice/Main.cpp
@@ -1,6 +1,22 @@
 #include "glut.h"
 
+//	描画が必要になったら
+void display()
+{
+
+}
+
 int main(int argc, char *argv[])
 {
 	glutInit(&argc,argv);
+
+
+	glutInitWindowPosition(640,0);			//	Window位置(やらなくてもいい)
+	glutInitWindowSize(640,640);			//	Window大きさ(やらなくてもいい)
+
+	glutCreateWindow("多々タイトル");		//	Windowのタイトル設定
+	glutDisplayFunc(display);				//	描画が必要になったら呼ばれるコールバックの設定
+	glutMainLoop();							//	処理をglutに委託する
+
+	return 0;
 }
